e7738f3ac74c0453f977ecd5c833a2bd61c52ade
Peruzcodx <yeyebusiness123@gmail.com>
2026-09-15 11:35:35 +0100

    Configure API URL for deployment

--- dashboard/api.py
+++ dashboard/api.py
@@ -1,7 +1,15 @@
 import requests
 import streamlit as st
 
+import os
+import requests
+import streamlit as st
 
+
+API_BASE_URL = os.getenv(
+    "API_BASE_URL",
+    "http://127.0.0.1:8000",
+)
 API_BASE_URL = "http://127.0.0.1:8000"
 
 
